e03ad1faf31e54a222f86972dec1bb63054afec0
Mother <home@mothers-mbp.lan>
2019-11-01 09:42:30 +0100

    Vendredi

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-console.log('it works')
+console.log("it works");
+
+// const gestion = () => {
+//   let windowHeight = window.height();
+//   console.log(windowHeight);
+//   let windowWidth = $(window).width();
+//   console.log(windowWidth);
+//   window.onresize
+
+//   document.querySelector(".windowSize").textContent = "TEST";
+// };
+// window.addEventListener("resize", gestion);
+
+window.onresize = displayWindowSize;
+window.onload = displayWindowSize;
+
+function displayWindowSize() {
+  myWidth = window.innerWidth;
+  myHeight = window.innerHeight;
+  console.log(myHeight);
+  document.getElementById("windowSize").innerText = myWidth + "x" + myHeight;
+}
